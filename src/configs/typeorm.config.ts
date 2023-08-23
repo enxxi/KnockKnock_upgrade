3020761221683bda71335e3223ae296eecb4e7cd
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-08-23 21:23:03 +0900

    nest 초기 세팅

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,10 @@
+// export const typeORMConfig: TypeOrmModuleOptions = {
+//     type: "database type",
+//     host: "database host",
+//     port: int(database port),
+//     username: "database username",
+//     password: "database password",
+//     database: "database name",
+//     entities: [database에서 사용하는 entity들,,,],
+//     sychronize: false
+//   }
