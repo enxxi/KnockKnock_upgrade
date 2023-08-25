525b25d3d521cd3e4f092d09d5fba1edd9b57508
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-08-25 14:36:19 +0900

    Chore: nest 초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CardModule } from './card/card.module';
+import { ChatModule } from './chat/chat.module';
+import { CommentModule } from './comment/comment.module';
+import { FileModule } from './file/file.module';
+import { MessageModule } from './message/message.module';
+import { JoinModule } from './join/join.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     // TypeORMModule.forRoot(typeORMConfig),
-  UserModule],
+  UserModule,
+    CardModule,
+    ChatModule,
+    CommentModule,
+    FileModule,
+    MessageModule,
+    JoinModule,
+    PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
