771e3be7fd75e1d3e4aa94d85b6ecf79756c41d5
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-08-28 14:32:16 +0900

    feat: user 스키마 초안

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,13 +1,46 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class User {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  intra_id: string;
+  email: string;
+
+  @Column()
+  name: string;
 
   @Column()
   nickname: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  birthday: string;
+
+  @Column()
+  age: string;
+
+  @Column()
+  job: string;
+
+  @Column()
+  region: string;
+
+  @Column()
+  mbti: string;
+
+  @Column()
+  height: string;
+
+  @Column()
+  introduce: string;
+
+  @Column()
+  is_deleted: string;
 }
