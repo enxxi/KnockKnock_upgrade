8d49c9238de45ca787e4e84c2612b63b3c18e17e
[AI7] 정재훈 <wjdwogns120523@gmail.com>
2023-08-28 16:20:18 +0900

    feat: rds 연결성공

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "4801",
-    database: "postgres",
+    host:  process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true
   }
