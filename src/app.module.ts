ff1b5866f3258d94f4cc064ee811a5329fd945b9
[AI7] 정재훈 <wjdwogns120523@gmail.com>
2023-08-30 14:43:35 +0900

    chore:prettier 적용안되는 문제 수정중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
-import { UserModule } from './user/user.module';
 import { CardModule } from './card/card.module';
 import { ChatModule } from './chat/chat.module';
 import { CommentModule } from './comment/comment.module';
+import { typeORMConfig } from './configs/typeorm.config';
 import { FileModule } from './file/file.module';
-import { MessageModule } from './message/message.module';
 import { JoinModule } from './join/join.module';
+import { MessageModule } from './message/message.module';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forRoot(typeORMConfig), 
        UserModule,
     CardModule,
     ChatModule,
