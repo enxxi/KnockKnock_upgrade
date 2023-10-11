cd337ba52c71d5558e80cffd522c65da494d5ef4
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-11 22:41:48 +0900

    chore: 젠킨스 CI/CD 구축

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js', 'tsconfig.json'],
   rules: {
-    'prettier/prettier': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
