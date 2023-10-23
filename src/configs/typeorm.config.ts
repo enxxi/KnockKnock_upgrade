c5787c87bd520780a47c3f81bcf0edfece8a9492
[AI7] 정재훈 <wjdwogns120523@gmail.com>
2023-10-23 14:40:49 +0900

    chore:db 연결 확인

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,9 +4,11 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: ['dist/**/*/*.entity{.ts,.js}'],
   synchronize: true,
+  // logging: true,
+  ssl: false,
 };
