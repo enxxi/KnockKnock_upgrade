a4bff54a44a1f8862440fb8210c48aaae7764271
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-10-25 02:26:36 +0900

    fix: db 연결 오류 해결

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,14 +1,26 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: ['dist/**/*/*.entity{.ts,.js}'],
-  synchronize: true,
-  // logging: true,
-  ssl: false,
-};
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: ['dist/**/*/*.entity{.ts,.js}'],
+        synchronize: true,
+        // logging: true,
+      }),
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
